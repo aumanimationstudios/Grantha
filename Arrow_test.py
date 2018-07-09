6dcd3ca4c165cf53fed83796b09ab06d78e96d34
sanath.shetty <sanathshetty111@gmail.com>
2018-07-09 14:16:34 +0530

    added Arrrow_test, improved read

---
+++ Arrow_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.7
+# *-* coding: utf-8 *-*
+
+import datetime
+import arrow
+
+utc = arrow.utcnow()
+print(utc)
+
+local = utc.to('Asia/Kolkata')
+print(local)
+
+print(local.timestamp)
+
+print(local.format())
+
+print(local.format('DD-MM-YYYY'))
+
+print(local.humanize())
+
+print(arrow.get(datetime.date(2018, 7, 1)))
+
--- Read.py
+++ Read.py
@@ -3,16 +3,33 @@
 
 import MySQLdb
 import sys
+import datetime
+import arrow
+from datetime import date
+from datetime import time
 
 db = MySQLdb.connect("localhost", "test", "test123", "INVENTORY")
 cursor = db.cursor()
 
-sql = "SELECT * FROM ITEMS"
+sql = """SELECT * FROM ITEMS"""
+
 try:
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
-        print row[0:9]
+        item_id = row[0]
+        serial_no = row[1]
+        model = row[2]
+        make = row[3]
+        purchased_on = row[4]
+        warranty_valid_till = row[5]
+        item_type = row[6]
+        location = row[7]
+        user = row[8]
+        print "item_id=%s,serial_no=%s,model=%s,make=%s,purchased_on=%s,warranty_valid_till=%s,item_type=%s," \
+              "location=%s,user=%s" %(item_id,serial_no,model,make,purchased_on,warranty_valid_till,item_type,
+                                      location,user)
+        #print row[0:9]
 except:
     print "Error: unable to fetch data : "+ str(sys.exc_info())
 
