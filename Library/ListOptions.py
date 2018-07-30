8738ab2929aa940542a9c9f39a8a0333abf8a2db
sanath111 <sanathshetty111@gmail.com>
2018-07-30 19:57:48 +0530

    added listOptions file

---
+++ Library/ListOptions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python2.7
+# *-* coding: utf-8 *-*
+
+import MySQLdb
+import sys
+import os
+import readchar
+
+filePath = os.path.abspath(__file__)
+progPath = os.sep.join(filePath.split(os.sep)[:-2])
+libraryPath = os.path.join(progPath,"Library","External_Modules")
+sys.path.append(libraryPath)
+
+from tabulate import tabulate
+
+def readList(option):
+    db = MySQLdb.connect("localhost","test","test123","INVENTORY")
+    cursor = db.cursor()
+
+    if (option == 'location'):
+        while True:
+            key = readchar.readkey()
+            if(key == 'l'):
+                cursor.execute("SELECT location FROM LOCATION")
+                results = cursor.fetchall()
+                print tabulate(results)
+                break
+            elif readchar.key.ENTER:
+                break
+            else:
+                print("Press a valid key")
+    elif (option == 'item_type'):
+         while True:
+            key = readchar.readkey()
+            if(key == 'l'):
+                cursor.execute("SELECT * FROM ITEM_TYPE")
+                results = cursor.fetchall()
+                print tabulate(results)
+                break
+            elif readchar.key.ENTER:
+                break
+            else:
+                print("Press a valid key")
+    elif(option == 'user'):
+        while True:
+            key = readchar.readkey()
+            if(key == 'l'):
+                cursor.execute("SELECT * FROM USER")
+                results = cursor.fetchall()
+                print tabulate(results)
+                break
+            elif readchar.key.ENTER:
+                break
+            else:
+                print("Press a valid key")
+
