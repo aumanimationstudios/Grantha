fdd3c1e02288724f7e3f8b92017e486ce88bbb96
sanath111 <sanathshetty111@gmail.com>
2018-08-06 11:31:09 +0530

    minor changes

--- Library/Delete.py
+++ Library/Delete.py
@@ -19,7 +19,7 @@ print CGREEN + "Enter the Serial_no of the item to delete " + CEND
 #user input
 sln = str(raw_input("Serial_no: "))
 
-print "Are You sure to delete the Item? [y/n]"
+print "Are You sure you want to delete this item? [y/n]"
 db = MySQLdb.connect("localhost", "test", "test123", "INVENTORY")
 db.autocommit(1)
 cursor = db.cursor()
@@ -29,6 +29,7 @@ while True:
     if(key == 'y'):
         try:
             cursor.execute("DELETE FROM ITEMS WHERE serial_no = '%s' " %(sln))
+            cursor.execute("DELETE FROM SERIAL_NO WHERE serial_no = '%s' " %(sln))
             print("Item with serial_no: "+ sln + " Deleted ")
         except:
             print "Error: unable to fetch data : "+ str(sys.exc_info())
@@ -36,7 +37,9 @@ while True:
     elif(key == 'n'):
         print "Nothing to do. Exiting..."
         break
+    else:
+        print("Press 'y' or 'n' ")
 
-    cursor.close()
-    db.close()
+cursor.close()
+db.close()
 
