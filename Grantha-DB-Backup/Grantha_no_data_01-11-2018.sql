1e4990affa3f3edf5a72f2e496b74f481dc2d746
sanath.shetty <sanathshetty111@gmail.com>
2019-02-23 12:32:47 +0530

    added database backup files

---
+++ Grantha-DB-Backup/Grantha_no_data_01-11-2018.sql
@@ -0,0 +1,182 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: INVENTORY
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `AUTH_USERS`
+--
+
+DROP TABLE IF EXISTS `AUTH_USERS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AUTH_USERS` (
+  `auth_users` varchar(100) NOT NULL,
+  PRIMARY KEY (`auth_users`),
+  UNIQUE KEY `auth_users_UNIQUE` (`auth_users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `DESCRIPTION`
+--
+
+DROP TABLE IF EXISTS `DESCRIPTION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DESCRIPTION` (
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`description`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ITEMS`
+--
+
+DROP TABLE IF EXISTS `ITEMS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ITEMS` (
+  `item_id` int(50) NOT NULL AUTO_INCREMENT,
+  `serial_no` varchar(100) NOT NULL,
+  `item_type` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `make` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `price` decimal(13,2) NOT NULL,
+  `purchased_on` date NOT NULL,
+  `warranty_valid_till` date NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `serial_no_UNIQUE` (`serial_no`),
+  KEY `item_type_idx` (`item_type`),
+  KEY `location_idx` (`location`),
+  KEY `user_idx` (`user`),
+  CONSTRAINT `item_type` FOREIGN KEY (`item_type`) REFERENCES `ITEM_TYPE` (`item_type`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `location` FOREIGN KEY (`location`) REFERENCES `LOCATION` (`location`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `USER` (`user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ITEM_TYPE`
+--
+
+DROP TABLE IF EXISTS `ITEM_TYPE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ITEM_TYPE` (
+  `item_type` varchar(100) NOT NULL,
+  PRIMARY KEY (`item_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `LOCATION`
+--
+
+DROP TABLE IF EXISTS `LOCATION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `LOCATION` (
+  `location` varchar(100) NOT NULL,
+  `parent_location` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`location`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `MAKE`
+--
+
+DROP TABLE IF EXISTS `MAKE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `MAKE` (
+  `make` varchar(100) NOT NULL,
+  PRIMARY KEY (`make`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `MODEL`
+--
+
+DROP TABLE IF EXISTS `MODEL`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `MODEL` (
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `SERIAL_NO`
+--
+
+DROP TABLE IF EXISTS `SERIAL_NO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SERIAL_NO` (
+  `serial_no` varchar(100) NOT NULL,
+  PRIMARY KEY (`serial_no`),
+  UNIQUE KEY `serial_no_UNIQUE` (`serial_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UPDATE_LOG`
+--
+
+DROP TABLE IF EXISTS `UPDATE_LOG`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UPDATE_LOG` (
+  `no` int(45) NOT NULL AUTO_INCREMENT,
+  `date_time` datetime(6) NOT NULL,
+  `serial_no` varchar(100) NOT NULL,
+  `old_location` varchar(100) NOT NULL,
+  `new_location` varchar(100) NOT NULL,
+  `updated_by` varchar(100) NOT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `user` varchar(100) NOT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-01  9:29:36
