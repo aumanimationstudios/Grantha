3be00955134a39eeb8146318a7bcc8c7d825fcd2
sanath.shetty <sanathshetty111@gmail.com>
2019-06-26 14:54:52 +0530

    code cleanup

--- GUI/Grantha.py
+++ GUI/Grantha.py
@@ -105,9 +105,6 @@ class mainWindow():
         # self.db = database.DataBase()
 
     def viewParentPopUp(self,pos):
-        # a = self.ui.tableWidget.horizontalHeaderItem(8).text()
-        #
-        # debug.info a
 
         selectedCellIndex = self.ui.tableWidget.selectedIndexes()
         for index in selectedCellIndex:
@@ -118,9 +115,7 @@ class mainWindow():
 
             if (selectedColumnLabel == "location"):
                 menu = QtWidgets.QMenu()
-                # view = QtWidgets.QMenu()
-                # view.setTitle("view parent location")
-                # menu.addMenu(view)
+
                 try:
                     selected = self.ui.tableWidget.selectedItems()
                 except:
@@ -148,12 +143,7 @@ class mainWindow():
     def viewParent(self):
         selectedText = self.ui.tableWidget.currentItem().text()
         # debug.info selectedText
-        # loc = self.db.listOfLocation()
-        # getLOC = "SELECT location FROM LOCATION"
-        # loc = self.db.execute(getLOC,dictionary=True)
-        # debug.info(loc)
-        # LOC = [x['location'] for x in loc]
-        # debug.info LOC
+
         if selectedText in self.LOC:
             getParentLocation = "SELECT parent_location FROM LOCATION WHERE location='%s' " %(selectedText)
             # pL = self.db.getParentLocation(query)
@@ -171,12 +161,6 @@ class mainWindow():
         else:
             debug.info ("not valid location")
 
-    # def viewParentMessage(self):
-    #     msg = QtWidgets.QMessageBox()
-    #     msg.setIcon(QtWidgets.QMessageBox.Information)
-    #     msg.setWindowTitle("Message")
-    #     msg.setText(self.parentMessage)
-    #     msg.exec_()
 
     def center(self):
         qr = self.ui.frameGeometry()
@@ -188,39 +172,23 @@ class mainWindow():
     def allBtnClick(self):
         # self.ui.tableWidget.setSortingEnabled(False)
         # self.ui.tableWidget.resizeColumnsToContents()
-        #db = database.DataBase()
         self.ui.tableWidget.setRowCount(0)
 
         self.ui.comboBox.clearEditText()
-        # column = self.db.getColumns()
-        # theColumn = [x['COLUMN_NAME'] for x in column]
-        # queryCol = "SELECT (COLUMN_NAME) FROM INFORMATION_SCHEMA.COLUMNS \
-        #             WHERE TABLE_NAME = 'ITEMS' AND COLUMN_NAME NOT IN ('item_id')"
-        #
-        # # debug.info(queryCol)
-        # column = self.db.execute(queryCol, dictionary=True)
-        # theColumn = [x['COLUMN_NAME'] for x in column]
-        # # debug.info(theColumn)
 
         self.ui.tableWidget.setColumnCount(len(self.theColumn))
         self.ui.tableWidget.setHorizontalHeaderLabels(self.theColumn)
 
         queryAll = "SELECT * FROM ITEMS ORDER BY item_type"
-        # theRows = self.db.getAllRows()
         theRows = self.db.execute(queryAll,dictionary=True)
         # debug.info(len(theRows))
         self.ui.tableWidget.setRowCount(len(theRows))
 
         row = 0
-        # self.db.getAllValues(init=True)
         while True:
             if (row == len(theRows)):
                 break
-            # primaryResult = self.db.getAllValues()
             primaryResult = theRows[row]
-            # debug.info (primaryResult)
-            # if (not primaryResult):
-            #     break
             col = 0
             for n in self.theColumn:
                 result = primaryResult[n]
@@ -244,21 +212,9 @@ class mainWindow():
         debug.info( "Loaded list of all items.")
         # self.ui.tableWidget.setSortingEnabled(True)
 
-    # def imageWidgetClicked(self, imagePath):
-    #     image_path = str(imagePath)
-    #     debug.info(image_path)
-    #     debug.info("Image Clicked")
-    #
-    #     # cmdFull = "xdg-open \"" + image_path + "\""
-    #     cmdFull = "feh \"" + image_path + "\" -Z -."
-    #     debug.info(cmdFull)
-    #     subprocess.Popen(cmdFull, shell=True)
 
     def search(self):
         self.ui.tableWidget.setRowCount(0)
-
-        # column = self.db.getColumns()
-        # self.theColumn = [x['COLUMN_NAME'] for x in column]
 
         self.ui.tableWidget.setColumnCount(len(self.theColumn))
         self.ui.tableWidget.setHorizontalHeaderLabels(self.theColumn)
@@ -307,8 +263,6 @@ class mainWindow():
             else:
                 pass
 
-        # else:
-        #     self.message()
 
     def fillTable(self,rows):
         row = 0
@@ -354,44 +308,20 @@ class mainWindow():
     def itBtnClick(self):
         self.ui.comboBox.clear()
         self.ui.comboBox.clearEditText()
-        # theList = self.db.Completer()
-        # itList = list(set([x['item_type'] for x in theList]))
         self.itList.sort()
         self.ui.comboBox.addItems(self.itList)
-        # self.model = QtCore.QStringListModel()
-        # self.model.setStringList(itList)
-        # self.completer()
 
     def locBtnClick(self):
         self.ui.comboBox.clear()
         self.ui.comboBox.clearEditText()
-        # theList = self.db.Completer()
-        # locList = list(set([x['location'] for x in theList]))
         self.locList.sort()
         self.ui.comboBox.addItems(self.locList)
-        # self.model = QtCore.QStringListModel()
-        # self.model.setStringList(locList)
-        # self.completer()
 
     def usrBtnClick(self):
         self.ui.comboBox.clear()
         self.ui.comboBox.clearEditText()
-        # theList = self.db.Completer()
-        # usrList = list(set([x['user'] for x in theList]))
         self.usrList.sort()
         self.ui.comboBox.addItems(self.usrList)
-        # self.model = QtCore.QStringListModel()
-        # self.model.setStringList(usrList)
-        # self.completer()
-
-    # def completer(self):
-    #     completer = QtWidgets.QCompleter()
-    #     completer.setModel(self.model)
-    #     completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
-    #     self.ui.comboBox.setCompleter(completer)
-
-    # def message(self):
-    #     QtWidgets.QMessageBox.about(QtWidgets.QMessageBox(),"Error!","Please Check Input.")
 
 
     # Processes to start when respective buttons are clicked
@@ -498,37 +428,14 @@ class mainWindow():
         messagebox = TimerMessageBox(1, plceMsg)
         messagebox.exec_()
 
-    # def openPlaceTagMessage(self):
-    #     self.plcMsg = QtWidgets.QMessageBox()
-    #     self.plcMsg.setIcon(QtWidgets.QMessageBox.Information)
-    #     self.plcMsg.setWindowTitle("Message")
-    #     self.plcMsg.setText("Place your Tag...")
-    #     self.plcMsg.show()
 
     def closePlaceTagMessage(self, tagId):
         try:
-            # self.plcMsg.close()
-            # debug.info(widgets)
-            # # # for childQWidget in self.ui.findChildren(QtWidgets.QWidget):
-            # # #     childQWidget.close()
-            # # # self.isDirectlyClose = True
-            # # # return QtGui.QMainWindow.close(self)
-            # btn = self.msg.defaultButton()
-            # debug.info(btn)
-            # for openWidgets in widgets:
-            #     # openWidgets.close()
-            #     self.msg.close(openWidgets)
-            # self.plcMsg.close()
-            # self.msg.close()
             debug.info("Message Closed")
         except:
             debug.info (str(sys.exc_info()))
             pass
 
-        # sn = self.db.listOfSerialNo()
-        # SN = [x['serial_no'] for x in sn]
-        # ti = self.db.listOfSerialNo()
-        # debug.info (ti)
         TI = [x['tag_id'] for x in self.sn]
         # debug.info  (TI)
         if tagId in TI:
@@ -541,16 +448,11 @@ class mainWindow():
             debug.info ("received sl.no: "+slNo)
             self.ui.comboBox.setEditText(slNo)
 
-            # column = self.db.getColumns()
-            # self.theColumn = [x['COLUMN_NAME'] for x in column]
-
             self.ui.tableWidget.setColumnCount(len(self.theColumn))
             self.ui.tableWidget.setHorizontalHeaderLabels(self.theColumn)
 
             currTxt = self.ui.comboBox.currentText().strip()
 
-            # self.query = "SELECT " + ','.join(self.theColumn) + " FROM ITEMS WHERE serial_no='%s' " %(currTxt)
-            # rows = self.db.getRows(self.query)
             getRows = "SELECT " + ','.join(self.theColumn) + " FROM ITEMS WHERE serial_no='%s' " % (currTxt)
             rows = self.db.execute(getRows, dictionary=True)
 
@@ -565,15 +467,6 @@ class mainWindow():
             self.ui.readMultiButton.setEnabled(True)
             # self.wrongTagMessage()
 
-    # def wrongTagMessage(self):
-    #     # self.Msg = QtWidgets.QMessageBox()
-    #     # self.Msg.setIcon(QtWidgets.QMessageBox.Information)
-    #     # self.Msg.setWindowTitle("Wrong Tag")
-    #     # self.Msg.setText("This Serial No. does not exists in Database \n And/Or \n Tag was not scanned properly!")
-    #     # self.Msg.show()
-    #     messageBox("This Serial No. does not exists in Database \n And/Or \n Tag was not scanned properly!",os.path.join(imgFilePath,"oh.png"))
-    #     self.ui.readSingleButton.setEnabled(True)
-    #     self.ui.readMultiButton.setEnabled(True)
 
     def readMultiFromRfidTag(self):
         self.ui.tableWidget.setRowCount(0)
@@ -605,10 +498,7 @@ class mainWindow():
             pass
 
         else:
-            # ti = self.db.listOfSerialNo()
-            # # debug.info (ti)
-            # TI = [x['tag_id'] for x in ti]
-            # debug.info  (TI)
+
             TI = [x['tag_id'] for x in self.sn]
             if tagId in TI:
                 # slno = self.db.getSlFrmTid(tagId)
@@ -644,20 +534,6 @@ class mainWindow():
 
                 rowCount = self.ui.tableWidget.rowCount()
 
-                # row = rowCount-1
-                #
-                # self.db.getValues(self.query, init=True)
-                # while True:
-                #     primaryResult = self.db.getValues(self.query)
-                #     # debug.info primaryResult
-                #     if (not primaryResult):
-                #         break
-                #     col = 0
-                #     for n in self.theColumn:
-                #         result = primaryResult[n]
-                #         self.ui.tableWidget.setItem(row, col, QtWidgets.QTableWidgetItem(str(result)))
-                #         col += 1
-                #     row += 1
 
                 row = rowCount -1
                 while True:
@@ -671,17 +547,6 @@ class mainWindow():
                     row += 1
                     break
 
-                # numRows = self.ui.tableWidget.rowCount()
-                # for row in range(numRows):
-                #     path = str(self.ui.tableWidget.item(row, 10).text())
-                #     debug.info(path)
-                #     self.ui.tableWidget.takeItem(row, 10)
-                #     imageThumb = ImageWidget(path, 32)
-                #     imageThumb.clicked.connect(lambda x, imagePath=path: imageWidgetClicked(imagePath))
-                #     self.ui.tableWidget.setCellWidget(row, 10, imageThumb)
-                #
-                # self.ui.tableWidget.resizeColumnsToContents()
-                # self.ui.tableWidget.resizeRowsToContents()
                 numRow = self.rfidMultiCount -1
                 path = str(self.ui.tableWidget.item(numRow, 10).text())
                 if path:
@@ -696,13 +561,7 @@ class mainWindow():
 
             else:
                 pass
-                # self.ui.readMultiButton.setEnabled(True)
 
-        # else:
-        #     self.wrongTagMessage()
-
-    # def readButtonEnable(self):
-    #     self.ui.readMultiButton.setEnabled(True)
 
     def stopRead(self):
         # self.ui.readMultiButton.setEnabled(True)
@@ -819,12 +678,6 @@ class readMultiThread(QThread):
                 # sock.shutdown(1)
                 sock.close()
                 break
-            # if(rep == "STOP"):
-            #     connection.close()
-            #     sock.shutdown()
-            #     sock.close()
-            #     break
-
             else:
                 self.tagIdReceived.emit(tagId)
         # sock.close()
@@ -863,28 +716,9 @@ class stopReadThread(QThread):
 
 
 
-
-# class setValueToTimerThread(QThread):
-#     sending = pyqtSignal(int)
-#
-#     def __init__(self, to, parent):
-#         QThread.__init__(self,parent)
-#         self.to = to
-#
-#     def run(self):
-#         for n in range(0, self.to+1):
-#             self.sending.emit(n)
-#             time.sleep(1)
-
-
-
-
 if __name__ == '__main__':
     setproctitle.setproctitle("GRANTHA")
 
-    # sys.stdout = open(tempDir + os.sep + "Grantha_" + user + ".log", 'w')
-    # print('test')
-    # tempDir + os.sep + "Grantha_ManageItems_" + user + ".log"
     app = QtWidgets.QApplication(sys.argv)
     window = mainWindow()
     sys.exit(app.exec_())
